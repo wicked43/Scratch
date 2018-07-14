186455a3565c5ea2a8dce110e5d9efe8f9c2b470
wicked <wicked-style@hotmail.de>
2018-07-14 18:38:42 +0200

    add Nand and Nor

--- logic_gates.py
+++ logic_gates.py
@@ -21,22 +21,22 @@ class BinaryGate(LogicGate):
         self.pinB = None
 
     def getPinA(self):
-        if self.pinA == None:
+        if self.pinA is None:
             return int(input("Enter Pin A input for gate " + self.getName() + "-->"))
         else:
             return self.pinA.getFrom().getOutput()
 
     def getPinB(self):
-        if self.pinB == None:
+        if self.pinB is None:
             return int(input("Enter Pin B input for gate " + self.getName() + "-->"))
         else:
             return self.pinB.getFrom().getOutput()
 
     def setNextPin(self, source):
-        if self.pinA == None:
+        if self.pinA is None:
             self.pinA = source
         else:
-            if self.pinB == None:
+            if self.pinB is None:
                 self.pinB = source
             else:
                 print("Cannot Connect: NO EMPTY PINS on this gate")
@@ -80,13 +80,13 @@ class UnaryGate(LogicGate):
         self.pin = None
 
     def getPin(self):
-        if self.pin == None:
+        if self.pin is None:
             return int(input("Enter Pin input for gate " + self.getName() + "-->"))
         else:
             return self.pin.getFrom().getOutput()
 
     def setNextPin(self, source):
-        if self.pin == None:
+        if self.pin is None:
             self.pin = source
         else:
             print("Cannot Connect: NO EMPTY PINS on this gate")
@@ -99,6 +99,36 @@ class NotGate(UnaryGate):
 
     def performGateLogic(self):
         if self.getPin():
+            return 0
+        else:
+            return 1
+
+
+class NandGate(BinaryGate):
+
+    def __init__(self, n):
+        BinaryGate.__init__(self, n)
+
+    def performGateLogic(self):
+        a = self.getPinA()
+        b = self.getPinB()
+
+        if a == 1 and b == 1:
+            return 0
+        else:
+            return 1
+
+
+class NorGate(BinaryGate):
+
+    def __init__(self, n):
+        BinaryGate.__init__(self, n)
+
+    def performGateLogic(self):
+        a = self.getPinA()
+        b = self.getPinB()
+
+        if a == 1 or b == 1:
             return 0
         else:
             return 1
