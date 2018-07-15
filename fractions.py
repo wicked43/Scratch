b0a8c5d99cd477661092db92e23d034d76a98e01
wicked <wicked-style@hotmail.de>
2018-07-15 03:35:31 +0200

    added functions

--- fractions.py
+++ fractions.py
@@ -7,20 +7,28 @@ def reduc(m, n):
         n = old_m % old_n
     return n
 
+
 class Fraction:
 
     def __init__(self, top, bottom):
-        self.num = top
-        self.den = bottom
+        if isinstance(top, int) and isinstance(bottom, int):
+            pass
+        else:
+            raise ValueError("Values are no Integers!")
+
+        self.num = top // reduc(top, bottom)
+        self.den = bottom // reduc(top, bottom)
 
     def __str__(self):
+        return f'{self.num}/{self.den}'
+
+    def __repr__(self):
         return f'{self.num}/{self.den}'
 
     def __add__(self, other):
         new_num = self.num * other.den + other.num * self.den
         new_den = self.den * other.den
-        common = reduc(new_num, new_den)
-        return Fraction(new_num // common, new_den // common)
+        return Fraction(new_num, new_den)
 
     def __eq__(self, other):
         first_n = self.num * other.den
@@ -30,26 +38,40 @@ class Fraction:
     def __mul__(self, other):
         new_num = self.num * other.num
         new_den = self.den * other.den
-        common = reduc(new_num, new_den)
-        return Fraction(new_num // common, new_den // common)
+        return Fraction(new_num, new_den)
 
     def __truediv__(self, other):
         new_num = self.num * other.den
         new_den = self.den * other.num
-        common = reduc(new_num, new_den)
-        return Fraction(new_num // common, new_den // common)
+        return Fraction(new_num, new_den)
 
     def __sub__(self, other):
         new_num = self.num * other.den - other.num * self.den
         new_den = self.den * other.den
-        common = reduc(new_num, new_den)
-        return Fraction(new_num // common, new_den // common)
+        return Fraction(new_num, new_den)
 
     def __lt__(self, other):
         first_n = self.num / self.den
         sec_n = other.num / other.den
         return first_n < sec_n
 
+    def __ge__(self, other):
+        first_n = self.num / self.den
+        sec_n = other.num / other.den
+        return first_n >= sec_n
+
+    def __radd__(self, other):
+        new_num = self.num * other.den + other.num * self.den
+        new_den = self.den * other.den
+        return Fraction(new_num, new_den)
+
+    def __iadd__(self, other):
+        new_num = self.num * other.den + other.num * self.den
+        new_den = self.den * other.den
+        return Fraction(new_num, new_den)
 
 
-print(Fraction(4,6) < Fraction(3,6))
+tst = Fraction(1, 2)
+tst += Fraction(1, 3)
+print(Fraction(1, 2) + Fraction(1, 3))
+print(tst)
